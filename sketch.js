d2b809ebb32f27b31fa449812798f8a2bcb29f5b
gezuz <gezuzm@hotmail.com>
2018-04-28 13:47:54 -0500

    codigo funcional

--- sketch.js
+++ sketch.js
@@ -5,6 +5,8 @@ var yPosition = 0;
 
 function setup() {
 	createCanvas(600, 400);
+	base = new brakeout();
+
 	for(var i = 0; i <= 2; i++){
 		blocks[i] = [];
 		yPosition += 30;
@@ -16,6 +18,8 @@ function setup() {
 
 function draw() {
 	background(51);
+	base.show();
+
 	for(var i = 0; i < shoots.length; i++){
 		shoots[i].show();
 		shoots[i].move();
@@ -26,3 +30,16 @@ function draw() {
 		}
 	}
 }
+
+
+//function keyPressed(){
+	/*if(keyCode === UP_ARROW){
+		var shoot = new shooter(base.x + 30, height-20);
+		shoots.push(shoot);
+	}*/
+	/*if(keyCode === RIGHT_ARROW){
+		base.move(1);
+	}else if(keyCode === LEFT_ARROW){
+		base.move(-1);
+	}
+}*/
